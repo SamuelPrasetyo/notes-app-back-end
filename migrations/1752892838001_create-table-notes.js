fdbbe78751724a5204ff85fd3ba11ba0ce88b1cf
Samuel Prasetyo <sprasetyo0511@gmail.com>
2025-07-19 10:25:34 +0700

    Migrations Table notes

---
+++ migrations/1752892838001_create-table-notes.js
@@ -0,0 +1,37 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('notes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    tags: {
+      type: 'TEXT[]',
+      notNull: true,
+      default: '{}',
+    },
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('notes');
+};
