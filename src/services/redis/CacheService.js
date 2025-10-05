fb7445bfad2bdd5bf5ae82c04703d4f14021f38f
SamuelPrasetyo <sprasetyo0511@gmail.com>
2025-10-06 00:34:03 +0700

    Fix: Update Redis client configuration to use correct environment variables

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -4,7 +4,8 @@ class CacheService {
   constructor() {
     this._client = redis.createClient({
       socket: {
-        host: process.env.REDIS_SERVER,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
       },
     });
 
